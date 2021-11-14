a4b86f9b04529c8055fce6572af5149031b2b506
Donald W. Boulton <donaldboulton@gmail.com>
2021-11-14 10:26:35 -0600

    sharp twice in config

--- src/components/bio.js
+++ src/components/bio.js
@@ -35,7 +35,7 @@ const Bio = () => {
       <StaticImage
         className="bio-avatar"
         layout="fixed"
-        formats={["auto", "webp", "avif"]}
+        formats={["auto", "webp"]}
         src="../images/donald-boulton.jpg"
         width={50}
         height={50}
