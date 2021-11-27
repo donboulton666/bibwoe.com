490644e829aa1ea99369c9760f04117c1e050585
Donald W. Boulton <donaldboulton@gmail.com>
2021-11-27 10:36:58 -0600

    image to seo props, remove gatsby-plugin-netlify

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
@@ -72,6 +65,7 @@ const Seo = ({ description, lang, meta, title }) => {
 Seo.defaultProps = {
   lang: `en`,
   meta: [],
+  image: ``,
   description: ``,
 }
 
