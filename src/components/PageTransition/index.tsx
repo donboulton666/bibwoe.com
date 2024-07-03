1df5975cbeaaf81b22cbc294fa516092e6f60f65
Donald Boulton <157413980+donboulton666@users.noreply.github.com>
2024-07-03 17:22:34 -0500

    beams fix

---
+++ src/components/PageTransition/index.tsx
@@ -0,0 +1,31 @@
+/* https://maxschmitt.me/posts/nextjs-page-transitions-framer-motion */
+import * as React from 'react'
+import { forwardRef } from 'react'
+import { motion, HTMLMotionProps } from 'framer-motion'
+
+type PageTransitionProps = HTMLMotionProps<'div'>
+type PageTransitionRef = React.ForwardedRef<HTMLDivElement>
+
+function PageTransition({ children, ...rest }: PageTransitionProps, ref: PageTransitionRef) {
+  const onTheRight = { x: '100%' }
+  const inTheCenter = { x: 0 }
+  const onTheLeft = { x: '-100%' }
+
+  const transition = { duration: 0.6, ease: 'easeInOut' }
+
+  return (
+    <motion.div
+      className="max-h-max"
+      ref={ref}
+      initial={onTheRight}
+      animate={inTheCenter}
+      exit={onTheLeft}
+      transition={transition}
+      {...rest}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default forwardRef(PageTransition)
